3bb507cc08a0d4d133e144ffe3f14fe3fe8e1ab8
onkar69483 <onkar69483@gmail.com>
2025-11-05 09:42:49 +0530

    first commit

---
+++ Exp2/q2/sample.c
@@ -0,0 +1,12 @@
+// This is a single-line comment
+/* This is a
+   multi-line comment */
+
+int main() {
+    int a = 5;
+    float b = 10.5;
+    if (a < b) {
+        a = a + 1;
+    }
+    return 0;
+}
---
+++ Exp2/q3/sample.c
@@ -0,0 +1,7 @@
+int main(){
+	int a = 5, b = 6;
+	if(a > b) {
+		a++;
+	}
+	return 0;
+}
